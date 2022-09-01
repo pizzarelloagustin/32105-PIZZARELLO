f7d0976d6eff67841d7915c356fe4d58bff7a7c4
pizzarelloagustin <pizzarelloagustin@outlook.com>
2022-09-01 20:40:44 -0300

    desafio 3 final

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const express = require("express");
-
+const db = require("./db.js");
 const app = express();
+const productos = new db("productos");
 
-
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
     try {
-        return res.send("prueba");
+        return res.send("Home");
+    } catch (e) {
+        return res.status(e).send(e);
+    }
+});
+
+app.get("/productos", async (req, res) => {
+    try {
+        const data = await productos.getAll();
+        console.log(data)
+        return res.send(data);
+    } catch (e) {
+        return res.status(e).send(e);
+    }
+});
+
+app.get("/productoRandom", async (req, res) => {
+    try {
+        const numRandom = Math.floor((Math.random() * 3)+1);
+        const data = await productos.getById(numRandom);
+        return res.send(data);
     } catch (e) {
         return res.status(e).send(e);
     }
