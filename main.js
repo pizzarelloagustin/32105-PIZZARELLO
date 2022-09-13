d1306c3d10270ad9f4df42b205d98e3e1ae2046f
pizzarelloagustin <pizzarelloagustin@outlook.com>
2022-09-13 20:06:21 -0300

    desafio 4

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 const express = require("express");
-const db = require("./db.js");
 const app = express();
-const productos = new db("productos");
+const productosRouter = require("./productos");
 
-app.get("/", (req, res) => {
-    try {
-        return res.send("Home");
-    } catch (e) {
-        return res.status(e).send(e);
-    }
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/productos", async (req, res) => {
-    try {
-        const data = await productos.getAll();
-        console.log(data)
-        return res.send(data);
-    } catch (e) {
-        return res.status(e).send(e);
-    }
-});
+app.use("/", express.static(__dirname + "/public"));
 
-app.get("/productoRandom", async (req, res) => {
-    try {
-        const numRandom = Math.floor((Math.random() * 3)+1);
-        const data = await productos.getById(numRandom);
-        return res.send(data);
-    } catch (e) {
-        return res.status(e).send(e);
-    }
-});
-
-
+app.use("/api/productos", productosRouter);
 
 app.listen(8080, () => {
-    console.log("Iniciado");
+  console.log("Server iniciado");
 });
