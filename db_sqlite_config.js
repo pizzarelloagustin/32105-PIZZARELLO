f8e62a16dc755e1686de15c1502fcd9c22662c57
pizzarelloagustin <pizzarelloagustin@outlook.com>
2022-10-25 23:56:56 -0300

    desafio 9: sin filtros, limpio

--- db_sqlite_config.js
+++
@@ -1,8 +0,0 @@
-const sqlite_config = {
-    client: "sqlite3",
-    connection: {
-        filename: "./DB/eccomerce.sqlite",
-    },
-    useNullAsDefault: true,
-};
-export default sqlite_config;
